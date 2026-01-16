4a476ff30c63a7cab7cf69d67b26afb74c1ce6d2
Ronald Rodrigues Farias <ronald-farias@outlook.com>
2026-01-16 14:19:02 -0300

    refactor: increment loans features adding due_date and celery schedule task every 15 days

--- library/tasks.py
+++ library/tasks.py
@@ -1,7 +1,10 @@
-from celery import shared_task
+from celery import shared_task, current_app, Celery
+from celery.schedules import crontab
 from .models import Loan
 from django.core.mail import send_mail
 from django.conf import settings
+from datetime import datetime, timedelta
+
 
 @shared_task
 def send_loan_notification(loan_id):
@@ -10,7 +13,7 @@ def send_loan_notification(loan_id):
         member_email = loan.member.user.email
         book_title = loan.book.title
         send_mail(
-            subject='Book Loaned Successfully',
+            subject="Book Loaned Successfully",
             message=f'Hello {loan.member.user.username},\n\nYou have successfully loaned "{book_title}".\nPlease return it by the due date.',
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[member_email],
@@ -18,3 +21,31 @@ def send_loan_notification(loan_id):
         )
     except Loan.DoesNotExist:
         pass
+
+
+@shared_task
+def check_overdue_loans():
+    enddate = datetime.now() - timedelta(days=15)
+    loans = Loan.objects.filter(is_returned=False, due_date__lte=enddate)
+    try:
+        for loan in loans:
+            member_email = loan.member.user.email
+            book_title = loan.book.title
+            send_mail(
+                subject="Book Loaned Successfully",
+                message=f'Hello {loan.member.user.username},\n\nYou have successfully loaned "{book_title}".\nPlease return it by the due date.',
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[member_email],
+                fail_silently=False,
+            )
+    except Loan.DoesNotExist:
+        pass
+
+
+@current_app.on_after_configure.connect
+def setup_periodic_tasks(sender: Celery, **kwargs):
+    sender.add_periodic_task(
+        crontab(day_of_month=15),
+        check_overdue_loans(),
+        name="add every 15 days",
+    )
--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from .models import Book, Author, Member, Loan
+from django.contrib.auth import get_user_model
+from datetime import datetime, timedelta
 
 # Create your tests here.
+
+
+class TestLoanTask(TestCase):
+
+    def setUp(self):
+        self._user = get_user_model().objects.create(username="testuser")
+
+        self._author = Author.objects.create(first_name="Jhon", last_name="Doe")
+        self._book = Book.objects.create(
+            title="Test title 01",
+            author=self._author,
+            isbn="123",
+            genre=Book.GENRE_CHOICES[0][0],
+        )
+        self._member = Member.objects.create(user=self._user)
+        self._loan = Loan.objects.create(
+            book=self._book,
+            member=self._member,
+        )
+
+    def test_loan_due_date_filter_should_return_zero(self):
+        enddate = datetime.now() - timedelta(days=15)
+        loans = Loan.objects.filter(is_returned=False, due_date__lte=enddate)
+        self.assertEqual(len(loans), 0)
+
+    def test_loan_due_date_filter_should_return_one(self):
+        loan = Loan.objects.create(
+            book=self._book,
+            member=self._member,
+        )
+        loan.due_date = datetime.now()
+        loan.save()
+        enddate = datetime.now()
+        loans = Loan.objects.filter(is_returned=False, due_date__lte=enddate)
+        self.assertEqual(len(loans), 1)
+
+    def test_extend_loan_due_date(self):
+        self.client.force_login(self._user)
+        self._loan.due_date = datetime.now()
+        self._loan.save()
+        response = self.client.post(
+            f"/api/loans/{self._loan.id}/extend_due_date/", data={"additional_days": 7}
+        )
+        loan = response.json()
+        self.assertEqual(
+            loan["due_date"], (datetime.now() + timedelta(days=7)).date().isoformat()
+        )
